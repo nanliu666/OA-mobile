a369a18920ce0349c48c95853a6a68384668e058
dwq <d13189432359@163.com>
2020-07-07 19:17:45 +0800

    feat:  我的任务

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,5 +1,6 @@
 import user from './user.js'
 import message from './message.js'
-
+import metask from './metask'
 user({ mock: true })
 message({ mock: true })
+metask({ mock: true })
--- src/router/routes/me.js
+++ src/router/routes/me.js
@@ -1,44 +1,50 @@
-export default[
-    {
-        path: '/me/feedback',
-        name: '意见反馈',
-        component: () =>
-          import(/* webpackChunkName: "view" */ '@/view/me/feedback.vue'),
-        meta: { title: '个人中心' }
-      },
-      {
-        path: '/me/setting',
-        name: '设置',
-        component: () =>
-          import(/* webpackChunkName: "view" */ '@/view/me/setting.vue'),
-        meta: { title: '个人中心' }
-      },
-      {
-        path: '/me/accountSafe',
-        name: '账号与安全',
-        component: () =>
-          import(/* webpackChunkName: "view" */ '@/view/me/accountSafe.vue'),
-        meta: { title: '个人中心' }
-      },
-      {
-        path: '/me/changePhone',
-        name: '修改手机号码',
-        component: () =>
-          import(/* webpackChunkName: "view" */ '@/view/me/changePhone.vue'),
-        meta: { title: '个人中心' }
-      },
-      {
-        path: '/me/changePassword',
-        name: '修改登陆密码',
-        component: () =>
-          import(/* webpackChunkName: "view" */ '@/view/me/changePassword.vue'),
-        meta: { title: '个人中心' }
-      },
-      {
-        path: '/me/personalInfo',
-        name: '个人信息',
-        component: () =>
-          import(/* webpackChunkName: "view" */ '@/view/me/personalInfo.vue'),
-        meta: { title: '个人中心' }
-      },
+export default [
+  {
+    path: '/me/feedback',
+    name: '意见反馈',
+    component: () => import(/* webpackChunkName: "view" */ '@/view/me/feedback.vue'),
+    meta: { title: '个人中心' }
+  },
+  {
+    path: '/me/setting',
+    name: '设置',
+    component: () => import(/* webpackChunkName: "view" */ '@/view/me/setting.vue'),
+    meta: { title: '个人中心' }
+  },
+  {
+    path: '/me/accountSafe',
+    name: '账号与安全',
+    component: () => import(/* webpackChunkName: "view" */ '@/view/me/accountSafe.vue'),
+    meta: { title: '个人中心' }
+  },
+  {
+    path: '/me/changePhone',
+    name: '修改手机号码',
+    component: () => import(/* webpackChunkName: "view" */ '@/view/me/changePhone.vue'),
+    meta: { title: '个人中心' }
+  },
+  {
+    path: '/me/changePassword',
+    name: '修改登陆密码',
+    component: () => import(/* webpackChunkName: "view" */ '@/view/me/changePassword.vue'),
+    meta: { title: '个人中心' }
+  },
+  {
+    path: '/me/personalInfo',
+    name: '个人信息',
+    component: () => import(/* webpackChunkName: "view" */ '@/view/me/personalInfo.vue'),
+    meta: { title: '个人中心' }
+  },
+  {
+    path: '/me/myTask',
+    name: '我的任务',
+    component: () => import(/* webpackChunkName: "view" */ '@/view/me/myTask/myTask'),
+    meta: { title: '我的任务' }
+  },
+  {
+    path: '/me/taskDetail',
+    name: '我的任务',
+    component: () => import(/* webpackChunkName: "view" */ '@/view/me/myTask/taskDetail'),
+    meta: { title: '任务详情' }
+  }
 ]
