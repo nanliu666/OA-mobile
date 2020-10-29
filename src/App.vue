ca7eebba16901e7b626b20a4d185b35997ce8042
xiangrun <xiangrun.kong@epro.com.cn>
2020-10-29 10:26:11 +0800

    chore: 增加移动端调试工具

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,16 @@ export default {
   name: 'App',
   data() {
     return {}
+  },
+  created() {
+    // 当在开发环境并且是移动端时候开启移动调试台
+    let isMobile =
+      /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent) &&
+      navigator.platform !== 'Win32' &&
+      process.env.NODE_ENV === 'development'
+    if (isMobile) {
+      window.eruda.init()
+    }
   }
 }
 </script>
